ec8133dd170ab78922d7cb1fbaa9350c9beb4958
Pratham <udeshipratham@gmail.com>
2024-12-29 12:43:04 +0530

    changes

--- be/dist/index.js
+++ be/dist/index.js
@@ -13,15 +13,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
-const sdk_1 = __importDefault(require("@anthropic-ai/sdk"));
 const express_1 = __importDefault(require("express"));
+const sdk_1 = __importDefault(require("@anthropic-ai/sdk"));
 const prompts_1 = require("./prompts");
 const node_1 = require("./defaults/node");
 const react_1 = require("./defaults/react");
-// const CLAUDE_API_KEY = (process.env.CLAUDE_API_KEY)
+const cors_1 = __importDefault(require("cors"));
 const anthropic = new sdk_1.default();
 const app = (0, express_1.default)();
-app.listen(3000);
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const prompt = req.body.prompt;
@@ -36,38 +36,41 @@ app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* 
         max_tokens: 200,
         system: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
     });
-    const answer = response.content[0].text;
+    const answer = response.content[0].text; // react or node
     if (answer == "react") {
         res.json({
-            prompts: [prompts_1.BASE_PROMPT, react_1.basePrompt],
+            prompts: [
+                prompts_1.BASE_PROMPT,
+                `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+            ],
+            uiPrompts: [react_1.basePrompt],
         });
         return;
     }
-    if (answer == "node") {
+    if (answer === "node") {
         res.json({
-            prompts: [node_1.basePrompt],
+            prompts: [
+                `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+            ],
+            uiPrompts: [node_1.basePrompt],
         });
         return;
     }
-    res.status(403).json({ message: "You can't access this!" });
+    res.status(403).json({ message: "You cant access this" });
     return;
 }));
-// async function main() {
-//   anthropic.messages
-//     .stream({
-//       messages: [
-//         {
-//           role: "user",
-//           content:
-//             "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags.",
-//         },
-//       ],
-//       model: "claude-3-5-sonnet-20241022",
-//       max_tokens: 1024,
-//       system: getSystemPrompt(),
-//     })
-//     .on("text", (text) => {
-//       console.log(text);
-//     });
-// }
-// main();
+app.post("/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const messages = req.body.messages;
+    const response = yield anthropic.messages.create({
+        messages: messages,
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 8000,
+        system: (0, prompts_1.getSystemPrompt)(),
+    });
+    console.log(response);
+    res.json({
+        response: (_a = response.content[0]) === null || _a === void 0 ? void 0 : _a.text,
+    });
+}));
+app.listen(3000);
--- be/src/index.ts
+++ be/src/index.ts
@@ -1,16 +1,15 @@
 require("dotenv").config();
-import Anthropic from "@anthropic-ai/sdk";
 import express from "express";
+import Anthropic from "@anthropic-ai/sdk";
 import { BASE_PROMPT, getSystemPrompt } from "./prompts";
-import { TextBlock } from "@anthropic-ai/sdk/resources";
-import { basePrompt as nodebaseprompt } from "./defaults/node";
-import { basePrompt as reactbaseprompt } from "./defaults/react";
-// const CLAUDE_API_KEY = (process.env.CLAUDE_API_KEY)
+import { ContentBlock, TextBlock } from "@anthropic-ai/sdk/resources";
+import { basePrompt as nodeBasePrompt } from "./defaults/node";
+import { basePrompt as reactBasePrompt } from "./defaults/react";
+import cors from "cors";
 
 const anthropic = new Anthropic();
 const app = express();
-
-app.listen(3000);
+app.use(cors());
 app.use(express.json());
 
 app.post("/template", async (req, res) => {
@@ -34,9 +33,9 @@ app.post("/template", async (req, res) => {
     res.json({
       prompts: [
         BASE_PROMPT,
-        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactbaseprompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
       ],
-      uiPrompts: [reactbaseprompt],
+      uiPrompts: [reactBasePrompt],
     });
     return;
   }
@@ -44,9 +43,9 @@ app.post("/template", async (req, res) => {
   if (answer === "node") {
     res.json({
       prompts: [
-        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactbaseprompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
       ],
-      uiPrompts: [nodebaseprompt],
+      uiPrompts: [nodeBasePrompt],
     });
     return;
   }
@@ -71,22 +70,4 @@ app.post("/chat", async (req, res) => {
   });
 });
 
-// async function main() {
-//   anthropic.messages
-//     .stream({
-//       messages: [
-//         {
-//           role: "user",
-//           content:
-//             "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags.",
-//         },
-//       ],
-//       model: "claude-3-5-sonnet-20241022",
-//       max_tokens: 1024,
-//       system: getSystemPrompt(),
-//     })
-//     .on("text", (text) => {
-//       console.log(text);
-//     });
-// }
-// main();
+app.listen(3000);
